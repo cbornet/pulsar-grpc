7686f09ac09bb90f598d4c235b3163b417547ae8
cbornet <cbornet@hotmail.com>
2020-12-10 19:18:27 +0100

    Ignore flaky test in CI

--- protocol-handler/src/test/java/org/apache/pulsar/protocols/grpc/MessagePublishThrottlingTest.java
+++ protocol-handler/src/test/java/org/apache/pulsar/protocols/grpc/MessagePublishThrottlingTest.java
@@ -38,7 +38,6 @@ import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import java.util.Collections;
@@ -449,7 +448,6 @@ public class MessagePublishThrottlingTest extends ProducerConsumerBase {
      * @throws Exception
      */
     @Test
-    @Ignore
     public void testBrokerTopicPublishByteThrottling() throws Exception {
         log.info("-- Starting {} test --", methodName);
 
